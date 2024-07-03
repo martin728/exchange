31a9f0d6b9516d4bc0d60d0436afc138176d791b
diana.martyn <diana.martyn@ihsmarkit.com>
2024-07-03 17:29:37 +0300

    remove unused fields

--- src/app/constants.ts
+++ src/app/constants.ts
@@ -3,12 +3,7 @@ export const DEFAULT_GET_CURRENCY = 'UAH';
 export const MAX_AMOUNT = 1000000;
 
 export interface CurrencyDataResponse {
-  WARNING_UPGRADE_TO_V6: string;
-  base: string;
-  provider: string;
-  terms: string;
   rates: Rate;
-  time_last_update: string;
   date: string;
 }
 
